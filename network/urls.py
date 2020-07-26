d02ca7c850b42f8f7292a30adcb0e77d10171fa6
aali2537 <alexander_li1@student.uml.edu>
2020-07-26 03:22:23 -0400

    added newpost view

--- network/urls.py
+++ network/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("newpost", views.new_post, name="newPost"),
     path("register", views.register, name="register")
 ]
